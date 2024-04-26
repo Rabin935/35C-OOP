5509334c0fb697a623f3d16b3ee35f71a9ea116c
Rabin <lrabin935@gmail.com>
2024-04-26 19:02:47 +0545

    Week 2 task complete

--- Week 2/weektwo.java
+++ Week 2/weektwo.java
@@ -51,12 +51,12 @@ public class weektwo {
 
         System.out.println("breadth");
         double breadth = scan.nextDouble();
-        System.out.println("The area of cuboid is "+ (cuboid));
+        System.out.println("The volume of cuboid is "+ (cuboid));
 
         double triangle = (0.5 *breadth * height);
         System.out.println("The area of triangle is "+ (triangle));
 
-        System.out.println("The area of cube is "+ (cube));
+        System.out.println("The volume of cube is "+ (cube));
 
         /* 4. Write the ternary operator for question no. 1 */
 
@@ -119,8 +119,92 @@ public class weektwo {
 
         System.out.printf("Hey, my name is " + (name) + " and "+ " my roll number is "+ (roll_number) + ". \nMy field of interest are "+ (field)+".");
 
+        /* 8. Take side of a square from user and print area and perimeter of it. Also,
+         calculate Simple Interest, Area of triangle and Volume of Cube and Cuboid. 
+         Take the attributes as user input. */
+
+        System.out.println("Enter the side of the square:");
+        double squareSide = scan.nextDouble();
+        double squareArea = squareSide * squareSide;
+        double squarePerimeter = 4 * squareSide;
+        System.out.println("Area of the square: " + squareArea);
+        System.out.println("Perimeter of the square: " + squarePerimeter);
+
+        System.out.println("Enter principal: ");
+        double Principal = scan.nextDouble();
+
+        System.out.println("Enter rate (in percentage): ");
+        double Rate = scan.nextDouble();
+
+        System.out.println("Enter years: ");
+        double Time = scan.nextDouble();
+
+        double Simpleinterest = (Principal*Rate*Time)/ 100;
+        System.out.println("Simple interest = "+ (Simpleinterest));
+
+        System.out.println("Lenght: ");
+        int Length = scan.nextInt();
+
+        System.out.println("Height");
+        int Height = scan.nextInt();
+
+        System.out.println("width");
+        int Width = scan.nextInt();
+
+        int Cuboid = Length * Width * Height;
+        int Cube = Length * Length + Length;
+
+        System.out.println("breadth");
+        double Breadth = scan.nextDouble();
+        System.out.println("The volume of cuboid is "+ (Cuboid));
+
+        double Triangle = (0.5 *Breadth * Height);
+        System.out.println("The area of triangle is "+ (Triangle));
+
+        System.out.println("The volume of cube is "+ (Cube));
 
 
+        /* 9.  Ask user to give two double input for length and breadth of 
+        a rectangle and print area type casted to int. */
+
+        System.out.println("Enter your number: ");
+        double length_rec = scan.nextDouble();
+
+        System.out.println("Enter your number: ");
+        double breadth_rec = scan.nextDouble();
+
+        double Rectangle = length_rec * breadth_rec;
+        System.out.println("The area tpe casted to integer is "+ (Rectangle));
+
+        /* 10. Write a program to calculate the total marks of four subjects of a student 
+         * and the total percentage secured. And use the following conditions
+         * to generate the final result;
+         * a. If equal to or more than 70 -> First Class
+           b. If more than 59 -> Upper Second Class
+           c. If more than 49 -> Second class
+           d. If more than 39 -> Third class and if below than 40 the result is fail. 
+           Hint: Use ternary operator
+         */
+
+
+        System.out.println("Enter your marks: ");
+        int math = scan.nextInt();
+        int science = scan.nextInt();
+        int social = scan.nextInt();
+        int english = scan.nextInt();
+
+        int total_marks = math + science + social + english;
+        double percentage = (total_marks / 4.0) * 100;
+
+        String result = (percentage >=70) ? "First class"
+                        : (percentage > 59) ? "Upper Second class"
+                        : (percentage > 49) ? "Second class"
+                        : (percentage > 39) ? "Third class"
+                        : "Fail";
+
+        System.out.println("Total marks is "+(total_marks));
+        System.out.println("Total percentage "+ (percentage)+"%");
+        System.out.println("Final results is "+ (result));
 
 
         scan.close();
